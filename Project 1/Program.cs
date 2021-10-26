2faea94c7f7e4f7fc7848316763697ed6052f1a1
Lucas Smith <lsoceanengineer@gmail.com>
2021-10-25 20:38:12 -0700

    Add project files.

---
+++ Project 1/Program.cs
@@ -0,0 +1,95 @@
+﻿/// Week 9 Project 1
+/// File Name: week9project1.cs
+/// Author: Lucas Smith
+/// Date:  October 25, 2021
+///
+/// Problem Statement: Define a class called Odometer that will be used to track fuel usage and miles per gallon.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Declare public class Odometer
+/// 2) Declare the methods inside, along with the private scope variable count that will be used inside.
+/// 3) Test each method in the Main class
+
+using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing All Methods: ");
+
+            Odometer odo = new Odometer(19, 20);
+
+            odo.addFuel(9);
+
+            odo.drive(18);
+
+            Console.WriteLine(odo.getFuelEfficiency());
+            Console.WriteLine(odo.getFuelLevel());
+            Console.WriteLine(odo.getOdometer());
+
+
+            Console.WriteLine(odo.ToString());
+
+
+        }
+    }
+}
+
+
+class Odometer
+{
+    private double fuelLevel;
+    private double fuelEff;
+    private double odometer;
+
+    public Odometer(double fuelEff, double fuelLevel)
+    {
+        this.fuelLevel = fuelLevel;
+        this.fuelEff = fuelEff;
+        this.odometer = 0.0;
+    }
+
+    public double getFuelEfficiency()
+    {
+        return this.fuelEff;
+    }
+
+    public double getFuelLevel()
+    {
+        return this.fuelLevel;
+    }
+
+    public double getOdometer()
+    {
+        return this.odometer;
+    }
+
+    public double addFuel(double gallons)
+    {
+        this.fuelLevel = this.fuelLevel + gallons;
+        return this.fuelLevel;
+    }
+
+    public double drive(double miles)
+    {
+        double distance = (this.fuelLevel * this.fuelEff);
+
+        if (distance > miles)
+        {
+            distance = miles;
+        }
+
+        this.fuelLevel = (fuelLevel - (distance / fuelEff));
+        this.odometer = odometer + distance;
+        return distance;
+    }
+
+    public override String ToString()
+    {
+        return "MPG: " + this.fuelEff + " Miles: " + this.odometer + " Fuel: " + this.fuelLevel;
+    }
+}
---
+++ Project 2/Program.cs
@@ -0,0 +1,103 @@
+﻿/// Week 9 Project 2
+/// File Name: week9project2.cs
+/// Author: Lucas Smith
+/// Date:  October 25, 2021
+///
+/// Problem Statement: Write a Temperature class that has two instance variables: a temperature value (a floating-point number) and a character for the scale, either C for Celsius of F for Fahrenheit.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Declare public class Temperature
+/// 2) Declare the methods inside, along with the private scope variable count that will be used inside.
+/// 3) Test each method in the Main class
+/// 
+using System;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the scale (c for Celsius or f for Fahrenheit): ");
+            char scaleLetter = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter the temperature value: ");
+            double temperature = Convert.ToDouble(Console.ReadLine());
+
+            Temperature t = new Temperature(scaleLetter, temperature);
+
+            char oldScale = t.getScale();
+            double oldTemp = t.getTemp();
+            double newTemp;
+
+            Console.WriteLine(t.Equals(oldTemp));
+
+            Console.WriteLine("Set a new temperature: ");
+            newTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(t.Equals(newTemp));
+
+            Console.WriteLine("Set a new scale: ");
+            char newScale = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Now set both (Temp first): ");
+            double nextTemp = Convert.ToDouble(Console.ReadLine());
+            newScale = Convert.ToChar(Console.ReadLine());
+
+            t.SetBoth(nextTemp, newScale);
+
+            Console.WriteLine(t.ToString());
+
+
+
+        }
+    }
+}
+
+class Temperature
+{
+    private char scale = 'c';
+    private double temp = 0.0;
+
+    public Temperature(char sc, double tp)
+    {
+        scale = sc;
+        temp = tp;
+    }
+
+    public char getScale()
+    {
+        return scale;
+    }
+
+    public double getTemp()
+    {
+        return temp;
+    }
+
+    public void setTemp(double t)
+    {
+        temp = t;
+    }
+
+    public void setScale(char s)
+    {
+        scale = s;
+    }
+
+    public void SetBoth(double te, char se)
+    {
+        temp = te;
+        scale = se;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return temp.Equals(obj);
+    }
+    public override String ToString()
+    {
+        return "The temperature is " + temp + " " + scale + ".";
+    }
+}
